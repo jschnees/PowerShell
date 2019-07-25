61af7f7b79272a7b50c7a2f32a5df13cc3db5230
Justin Schnees <jschnees@users.noreply.github.com>
2019-07-25 11:07:52 -0400

    Update OgranizeCAD.ps1

--- OgranizeCAD.ps1
+++ OgranizeCAD.ps1
@@ -1,12 +1,20 @@
-# Finds files with names
-# Then moves a file to the newly created named folder
+$SourceFolder = "G:\queries\"
+$targetFolder = "G:\queries\"
 
+# Find all files matching *.sql in the folder specified
+Get-ChildItem -Path $SourceFolder -Filter *.sql | ForEach-Object {
 
-Get-ChildItem -File |  # Get files
-  Group-Object { $_.Name -replace '_.*' } |  # Group by part before first underscore
-  ForEach-Object {
-    # Create directory
-    $dir = New-Item -Type Directory -Name $_.Name
-    # Move files there
-    $_.Group | Move-Item -Destination $dir
-  }
+    # Combine the source filename and target directory
+    # The source filename has all instances of _ replaced with \
+    # Cast the resulting string to a FileInfo object to take advantage of extra methods
+    [System.IO.FileInfo]$destination = (Join-Path -Path $targetFolder -ChildPath $_.Name.replace("_","\"))
+
+    # Create the directory if it doesn't already exits
+    if (!(Test-Path) $destination.Directory.FullName)
+    { 
+        New-item -Path $destination.Directory.FullName -ItemType Directory 
+    }
+
+    # Copy the source to the target directory
+    copy-item -Path $_.FullName -Destination $Destination.FullName 
+}
