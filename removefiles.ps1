e113ba5f576e381592d9e7a75db2532682be6b48
Justin Schnees <jschnees@users.noreply.github.com>
2019-09-03 12:04:10 -0400

    Update removefiles.ps1

--- removefiles.ps1
+++ removefiles.ps1
@@ -1,5 +1,5 @@
-Get-ChildItem -Path 'Q:\projects\2019\19073 (MA) Doctors 1st Floor Restrooms and Main Entry Doors\00 - IN\2019-07-18 Revit\CAD' *.png | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem -Path 'C:\PATH' *.png | foreach { Remove-Item -Path $_.FullName }
 
-Get-ChildItem -Path 'Q:\projects\2019\19073 (MA) Doctors 1st Floor Restrooms and Main Entry Doors\00 - IN\2019-07-18 Revit\CAD' *.pcp | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem -Path 'C:\PATH' *.pcp | foreach { Remove-Item -Path $_.FullName }
 
-Get-ChildItem -Path 'Q:\projects\2019\19073 (MA) Doctors 1st Floor Restrooms and Main Entry Doors\00 - IN\2019-07-18 Revit\CAD' *.jpg | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem -Path 'C:\PATH' *.jpg | foreach { Remove-Item -Path $_.FullName }
